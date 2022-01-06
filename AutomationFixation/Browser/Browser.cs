a0c10fb45260316ce7283bd863fed820766a1ac0
Seth Sides <sethsides@live.com>
2022-01-06 10:44:14 -0600

    fixing namespace after adding folder structure

--- AutomationFixation/Browser/Browser.cs
+++ AutomationFixation/Browser/Browser.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Interactions;
-using OpenQA.Selenium.Support.Extensions;
 
-namespace AutomationFixation
+namespace AutomationFixation.Browser
 {
     public static class Browser
     {
